5289c81ada862180a5c4973a83a485000c84086c
MyOnAsSalat <89041155523@yandex.ru>
2023-10-02 09:11:48 +0800

    Time: 150 ms (36.52%), Space: 46 MB (91.67%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
@@ -0,0 +1,22 @@
+public class Solution {
+public int[] TopKFrequent(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var item in nums)
+        {
+            if (!dict.ContainsKey(item))
+            {
+                dict[item] = 0;
+            }
+            dict[item]++;
+        }
+        var dictList = dict.ToList();
+        dictList.Sort((pair1,pair2) => pair2.Value.CompareTo(pair1.Value));
+        var result = new int[k];
+        for (int i = 0; i < k; i++)
+        {
+            result[i] = dictList[i].Key;
+        }
+        return result;
+    }
+}
