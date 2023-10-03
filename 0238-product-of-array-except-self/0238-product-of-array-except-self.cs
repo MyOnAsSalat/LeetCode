2651fea826b0abb417d7f3d6c90b5bac2d397d44
MyOnAsSalat <deniska.xrenov23@yandex.ru>
2023-10-04 07:41:43 +0800

    Time: 144 ms (92.29%), Space: 54.3 MB (63.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,19 +1,10 @@
 public class Solution {
 public int[] ProductExceptSelf(int[] nums) {
-        bool zeroExist = false;
         var pref = 1;
         var post = 1;
         var result = new int[nums.Length];
         for (int i = 0; i < nums.Length; i++)
         {
-            if (nums[i] == 0)
-            {
-                if (zeroExist)
-                {
-                    return new int[nums.Length];
-                }
-                zeroExist = true;
-            }
             result[i] = pref;
             pref *= nums[i];
         }
